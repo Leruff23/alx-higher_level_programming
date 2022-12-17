f6a166910fd2c9b5289b706dab4982a8136abc5c
Leruff23 <juniorleruff01@hotmail.com>
2022-12-17 13:13:43 +0000

    fix 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
 
 
-
-
-
 def no_c(my_string):
 
-        my_list = []
+    my_list = []
 
-            for i in range(len(my_string)):
+    for i in range(len(my_string)):
 
-                        my_list.append(my_string[i])
+        my_list.append(my_string[i])
 
-                            for i in range(my_list.count('c')):
+    for i in range(my_list.count('c')):
 
-                                        my_list.remove('c')
+        my_list.remove('c')
 
-                                            for i in range(my_list.count('C')):
+    for i in range(my_list.count('C')):
 
-                                                        my_list.remove('C')
+        my_list.remove('C')
 
-                                                            new_string = ""
+    new_string = ""
 
-                                                                for i in range(len(my_list)):
+    for i in range(len(my_list)):
 
-                                                                            new_string = new_string + my_list[i]
+        new_string = new_string + my_list[i]
 
-                                                                                return new_string
+    return new_string
