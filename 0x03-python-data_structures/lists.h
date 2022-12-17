19b25e9629846b94857f15120ae1645c41481783
Leruff23 <juniorleruff01@hotmail.com>
2022-12-17 16:34:13 +0000

    fix 13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,12 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
 /**
- * struct listint_s - singly linked list
+
+ * struct listint_s - singly linked list.
  * @n: integer
- * @next: points to the next node
+ * @next: pointer to next node.
  *
- * Description: singly linked list node structure
+ * Description: singly linked node structure
+ * for project.
  */
 
 typedef struct listint_s
@@ -14,6 +21,7 @@ typedef struct listint_s
 int n;
 struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
