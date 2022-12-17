317fa3b3fb084c53ac0a855130d96e363e557e83
Leruff23 <juniorleruff01@hotmail.com>
2022-12-17 11:16:20 +0000

    fix task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
-for i in range(97, 123):
+for i in range(ord('a'), ord('z') + 1):
 
-        print("{:c}" .format(i), end="")
+    if chr(i) == 'e' or chr(i) == 'q':
+
+        continue
+
+    else:
+
+        print("{:s}".format(chr(i)), end="")
