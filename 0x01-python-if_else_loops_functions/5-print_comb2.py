69e0879fc013bf39197ec39c1641fe49ee7f97a1
Leruff23 <juniorleruff01@hotmail.com>
2022-12-17 11:28:39 +0000

    fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(100):
+for i in range(0, 99):
 
-        if i < 99:
+    print("{0:02d}".format(i), end=", ")
 
-                    print("{:02d}, " .format(i), end="")
-
-                        else:
-
-                                    print(i)
+print("{:d}".format(99))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 
-for i in range(10):
+for i in range(0, 9):
 
-        for j in range(10):
+    for j in range(i + 1, 10):
 
-                    if i >= j:
+        if i == 8 and j == 9:
 
-                                    continue
+            print("{:d}{:d}".format(i, j))
 
-                                            if i != 8 or j != 9:
+        else:
 
-                                                            print("{:d}{:d}, " .format(i, j), end="")
-
-                                                                    if i == 8 and j == 9:
-
-                                                                                    print("{:d}{:d}" .format(i, j))
+            print("{:d}{:d}".format(i, j), end=", ")
