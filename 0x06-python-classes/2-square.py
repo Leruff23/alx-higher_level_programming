de080e2368e01e1f408293f6eb54a1b98ca3d15e
Leruff23 <juniorleruff01@hotmail.com>
2023-01-03 22:49:26 +0000

    fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """Script that assigns a private instance attribute size to class
-
     Square with size validation"""
 
 
 class Square:
-
     """Class that defines a square with a private instance attribute size"""
 
 
